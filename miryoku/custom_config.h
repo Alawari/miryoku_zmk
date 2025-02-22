c8e911671f541be90d451169f21d711a9f14bd9c
Alawari <jeromjebai@web.de>
2025-02-22 01:36:05 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,7 +8,7 @@
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_MOUSE \
@@ -28,8 +28,8 @@ U_NP,              U_NP,              U_NA, U_NA, U_NA, U_LT(U_SYM, RET),  U_LT(
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  ESC \
-LSHFT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  ESC \
-LCTRL  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  ESC \
+ESC  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  ESC \
+ESC  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  ESC \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
