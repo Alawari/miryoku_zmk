b51ed99cbbf60a9a0ec75aa13419a2db89f9721f
Alawari <jeromjebai@web.de>
2025-02-23 01:04:08 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -195,4 +195,4 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 U_MT(SI_LGUI(LS(SI_Q))), SI_LGUI(SI_Q),            SI_LGUI(SI_W),       SI_LGUI(SI_E),       SI_LGUI(SI_R),       SI_LGUI(SI_T),       SI_LGUI(SI_Z),       SI_LGUI(SI_U),       SI_LGUI(SI_I),      SI_LGUI(SI_O),      SI_LGUI(SI_P),      \
 SI_LGUI(SI_A),			SI_LGUI(SI_S),            SI_LGUI(SI_D),       SI_LGUI(SI_F),       SI_LGUI(SI_G),       SI_LGUI(SI_H),       SI_LGUI(SI_J),       SI_LGUI(SI_K),       SI_LGUI(SI_L),      SI_LGUI(SI_OE),      \
 SI_LGUI(SI_Y),			SI_LGUI(SI_X),            SI_LGUI(SI_C),       SI_LGUI(SI_V),       SI_LGUI(SI_B),       SI_LGUI(SI_N),       U_MT(SI_LGUI(LS(SI_M)), SI_LGUI(SI_M)),       SI_LGUI(SI_COMMA),       SI_LGUI(SI_DOT),      SI_LGUI(SI_MINUS),     \
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), TAB, U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
