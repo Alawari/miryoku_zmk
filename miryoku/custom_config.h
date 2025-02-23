0186f3f1a48713014c4b47b3373a9c7a28427f44
Alawari <jeromjebai@web.de>
2025-02-23 12:25:47 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,12 +3,7 @@
  *
  * SPDX-License-Identifier: MIT
  */
-#pragma once
 
-#include <dt-bindings/zmk/hid_usage.h>
-#include <dt-bindings/zmk/hid_usage_pages.h>
-#include <dt-bindings/zmk/modifiers.h>
-#include <dt-bindings/zmk/keys.h>
 
 /*
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
