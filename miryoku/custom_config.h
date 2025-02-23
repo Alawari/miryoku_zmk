b71a1e49ce6fcca1e8bc66354e26612dd68e1a89
Alawari <jeromjebai@web.de>
2025-02-23 12:29:41 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -176,7 +176,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_
 
 
 #define MIRYOKU_LAYER_MOUSE \
-U_MT(LG(LS(Q)), LG(Q)),     U_MT(LG(LS(W)), LG(W)),     U_MT(LG(LS(E)), LG(E)),     U_MT(LG(LS(R)), LG(R)),     U_MT(LG(LS(T)), LG(T)),     U_MT(LG(LS(Z)), LG(Z)),     U_MT(LG(LS(U)), LG(U)),     U_MT(LG(LS(I)), LG(I)),     U_MT(LG(LS(O)), LG(O)),     U_MT(LG(LS(P)), LG(P)),    \
-U_MT(LG(LS(A)), LG(A)),     U_MT(LG(LS(S)), LG(S)),     U_MT(LG(LS(D)), LG(D)),     U_MT(LG(LS(F)), LG(F)),     U_MT(LG(LS(G)), LG(G)),     U_MT(LG(LS(H)), LG(H)),     U_MT(LG(LS(J)), LG(J)),     U_MT(LG(LS(K)), LG(K)),     U_MT(LG(LS(L)), LG(L)),     U_MT(LG(LS(RA(P))), LG(RA(P))),    \
-U_MT(LG(LS(Y)), LG(Y)),     U_MT(LG(LS(X)), LG(X)),     U_MT(LG(LS(C)), LG(C)),     U_MT(LG(LS(V)), LG(V)),     U_MT(LG(LS(B)), LG(B)),     U_MT(LG(LS(N)), LG(N)),     U_MT(LG(LS(M)), LG(M)),     U_MT(LG(SEMI), LG(COMMA)),     U_MT(LG(PERIOD), LG(COLON)),     U_MT(LG(UNDERSCORE), LG(MINUS)),    \
-U_NP,              U_NP,              U_NA, U_NA, U_NA, U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_MT(LA(LS(Q)), LA(Q)),            LA(W),       LA(E),       LA(R),       LA(T),       LA(Z),       LA(U),       LA(I),      LA(O),      LA(P),      \
+LA(A),			LA(S),            LA(D),       LA(F),       LA(G),       LA(H),       LA(J),       LA(K),       LA(L),      U_NA,      \
+LA(Y),			LA(X),            LA(C),       LA(V),       LA(B),       LA(N),       U_MT(LA(LS(M)), LA(M)),       LA(COMMA),       LA(DOT),      LA(MINUS),     \
+U_NP,              U_NP,               U_NA, U_NA, U_NA,, U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
